7ec418c70e8099dbfe030a783969a05b7cd5819b
Grig Alex <alexengrigdev@gmail.com>
2019-12-25 21:55:21 +0300

    Add 1/5 solution

--- Game of Life/task/src/life/Main.java
+++ Game of Life/task/src/life/Main.java
@@ -1,7 +1,54 @@
 package life;
 
+import java.util.Random;
+import java.util.Scanner;
+
+enum State {
+    ALIVE {
+        @Override
+        public String toString() {
+            return "O";
+        }
+    },
+    DEAD {
+        @Override
+        public String toString() {
+            return " ";
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        Cell[][] cells = new Cell[n][n];
+        int seed = scanner.nextInt();
+        Random random = new Random(seed);
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                State state = random.nextBoolean() ? State.ALIVE : State.DEAD;
+                cells[i][j] = new Cell(state);
+            }
+        }
+        for (Cell[] row : cells) {
+            for (Cell cell : row) {
+                System.out.print(cell);
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Cell {
+    final State state;
+
+    public Cell(State state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return state.toString();
     }
 }
