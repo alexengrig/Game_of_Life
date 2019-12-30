210686de81dde3bc3f779dc0fd168264656f30c6
Grig Alex <alexengrigdev@gmail.com>
2019-12-30 20:18:38 +0300

    Add "Finding and starting a thread" problem solution

---
+++ Problems (28)/Finding and starting a thread/src/Main.java
@@ -0,0 +1,9 @@
+public static void findAndStartThread(Thread... threads) throws InterruptedException {
+    for (Thread thread : threads) {
+        if (thread.getState() == Thread.State.NEW) {
+            thread.start();
+            thread.join();
+            break;
+        }
+    }
+}
